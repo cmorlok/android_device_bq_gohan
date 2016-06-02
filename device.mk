2ad01d601c858612e6dde22d54171df6a180c03e
TheStrix <parthbhatia98@gmail.com>
2016-06-02 14:45:37 +0530

    sensors: Switch to open MultiHAL sensor

--- device.mk
+++ device.mk
@@ -229,6 +229,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
 
+PRODUCT_PACKAGES += \
+    sensors.msm8952
 
 # Wifi
 PRODUCT_PACKAGES += \
